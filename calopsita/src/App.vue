a2b4f0b644addf0dc2927ecb850f30646a3926e4
Marco Bruno <marco.bruno.br@gmail.com>
2017-03-10 15:03:05 -0300

    [Cap. 3] - Integração com a API de terceiros

--- calopsita/src/App.vue
+++ calopsita/src/App.vue
@@ -15,17 +15,13 @@ export default {
   data () {
     return {
       titulo : 'Calopsita',
-      fotos : [
-        {
-          url : 'https://www.getninjas.com.br/assets/ninjas/eduardo-l-hotellier-hover-53f6b0833d5589eaee734213b995a22b6b75abd8a9d95ab6852da8d5f402d5ae.jpg',
-          titulo : 'Eduardo'
-        },
-        {
-          url : 'https://www.getninjas.com.br/assets/ninjas/enrique-marques-hover-c716a74edd4770c2e7c12dba65231771b7719f6b6779c95ebe48a0513feae5fe.jpg',
-          titulo : 'Enrique'
-        }
-      ]
+      fotos : []
     }
+  },
+  created() {
+    this.$http.get('http://localhost:3000/v1/fotos')
+              .then(res => res.json())
+              .then(fotos => this.fotos = fotos, err => console.log(err))
   }
 }
 </script>
