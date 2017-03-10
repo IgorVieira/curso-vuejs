66d63b0a609b15ffe9267027a7801ff00cce8b4a
Marco Bruno <marco.bruno.br@gmail.com>
2017-03-10 14:12:48 -0300

    [Cap. 2] - Adequando o template gerado

--- calopsita/src/App.vue
+++ calopsita/src/App.vue
@@ -1,30 +1,30 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1></h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+  <div>
+    <h1>{{ titulo }}</h1>
+
+    <ul v-for="foto of fotos">
+      <li>
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      titulo : 'Calopsita',
+      fotos : [
+        {
+          url : 'https://www.getninjas.com.br/assets/ninjas/eduardo-l-hotellier-hover-53f6b0833d5589eaee734213b995a22b6b75abd8a9d95ab6852da8d5f402d5ae.jpg',
+          titulo : 'Eduardo'
+        },
+        {
+          url : 'https://www.getninjas.com.br/assets/ninjas/enrique-marques-hover-c716a74edd4770c2e7c12dba65231771b7719f6b6779c95ebe48a0513feae5fe.jpg',
+          titulo : 'Enrique'
+        }
+      ]
     }
   }
 }
